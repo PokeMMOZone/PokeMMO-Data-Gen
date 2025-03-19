6b9a183a65fd886885581f6da489833a626f1c9e
Gamer2020 <lostatlife2020@hotmail.com>
2025-03-19 17:57:53 -0400

    start to download form images.

--- download_PokeAPI_sprites.py
+++ download_PokeAPI_sprites.py
@@ -7,8 +7,49 @@ from requests.exceptions import SSLError
 # Base URLs for the PokeAPI
 POKEMON_BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 POKEMON_SPECIES_URL = "https://pokeapi.co/api/v2/pokemon-species/"
+POKEMON_FORM_URL = "https://pokeapi.co/api/v2/pokemon-form/"
 DATA_SAVE_PATH = "./data/"
 SPRITES_FILE = "pokemon-sprites.json"
+
+EXCLUDED_VARIATION_PATTERNS = [
+    "-mega",
+    "-gmax",
+    "-alola",
+    "-hisui",
+    "-galar",
+    "-rock-star",
+    "-belle",
+    "-pop-star",
+    "-phd",
+    "-libre",
+    "-cosplay",
+    "-original-cap",
+    "-hoenn-cap",
+    "-sinnoh-cap",
+    "-unova-cap",
+    "-kalos-cap",
+    "-partner-cap",
+    "-starter",
+    "-world-cap",
+    "-primal",
+    "-paldea",
+    "-totem",
+    "palkia-origin",
+    "dialga-origin",
+    "basculin-white-striped",
+    "unown-a",
+    "arceus-normal",
+    "arceus-unknown",
+    "arceus-fairy",
+    "mothim-plant",
+    "pichu-spiky-eared",
+    "burmy-plant",
+    "cherrim-overcast",
+    "shellos-west",
+    "gastrodon-west",
+    "deerling-spring",
+    "sawsbuck-spring",
+]
 
 
 def request_with_retry(url):
@@ -88,12 +129,42 @@ def process_varieties(species_id):
     return []
 
 
+def process_forms(form_data):
+    processed_forms = []
+    for form in form_data:
+        form_name = form["name"]
+        if any(pattern in form_name for pattern in EXCLUDED_VARIATION_PATTERNS):
+            continue
+        form_response = request_with_retry(POKEMON_FORM_URL + form_name)
+        if form_response.status_code == 200:
+            form_json = form_response.json()
+            processed_forms.append({"name": form_name, "id": form_json["id"]})
+    return processed_forms
+
+
 def get_pokemon_sprites(pokemon_id):
     response = request_with_retry(POKEMON_BASE_URL + str(pokemon_id))
     if response.status_code == 200:
         pokemon_data = response.json()
         sprites = pokemon_data.get("sprites", {})
-        return {"id": pokemon_id, "name": pokemon_data["name"], "sprites": sprites}
+        return {
+            "id": pokemon_id,
+            "name": pokemon_data["name"],
+            "sprites": sprites,
+            "forms": pokemon_data.get("forms", []),
+        }
+    return None
+
+
+def get_pokemon_form_sprites(form_id):
+    response = request_with_retry(POKEMON_FORM_URL + str(form_id))
+    if response.status_code == 200:
+        form_data = response.json()
+        return {
+            "id": form_data["id"],
+            "name": form_data["name"],
+            "sprites": form_data.get("sprites", {}),
+        }
     return None
 
 
@@ -118,7 +189,15 @@ def main():
             for variety_id in varieties:
                 pokemon_sprites = get_pokemon_sprites(variety_id)
                 if pokemon_sprites:
+                    forms_info = process_forms(pokemon_sprites.get("forms", []))
+                    pokemon_sprites.pop("forms", None)
                     all_sprites_data[pokemon_sprites["name"]] = pokemon_sprites
+                    
+                    for form_info in forms_info:
+                        form_sprites = get_pokemon_form_sprites(form_info["id"])
+                        if form_sprites:
+                            if form_info["name"] not in all_sprites_data:
+                                all_sprites_data[form_info["name"]] = form_sprites
 
     save_sprites_data(all_sprites_data)
 
